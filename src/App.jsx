65e93c6785fb8b5fafa2d2ea7158d6a5f271ac6d
Sandeep Lal <sandeep221102@gmail.com>
2025-04-21 14:13:11 +0530

    setting up env

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
     <>
       <h1>Blog application with appwrite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
