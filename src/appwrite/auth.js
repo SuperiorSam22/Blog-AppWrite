c330fd2625c4fc244e6019e6d1c31cd204a67e19
Sandeep Lal <sandeep221102@gmail.com>
2025-04-21 15:20:24 +0530

    auth servie completed

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setEndpoint(config.appwriteProjectID);
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //call login method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, passowrd }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, passowrd);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service:: getCurrentUser :: error", error);
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
